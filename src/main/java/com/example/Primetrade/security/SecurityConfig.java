7e62ba859a808d21c8e01b98ed788b70785d10a2
Suensa01 <suensa389@gmail.com>
2026-02-21 16:49:33 +0530

    permit

--- src/main/java/com/example/Primetrade/security/SecurityConfig.java
+++ src/main/java/com/example/Primetrade/security/SecurityConfig.java
@@ -34,7 +34,11 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(
                                 SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**")
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
